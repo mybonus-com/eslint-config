ac95235afa6fcbbf73df062ddc98f320ecd5def4
Johan Kanefur <johan@kanefur.se>
2020-09-30 11:50:00 +0200

    feat(rules): Add import order rules
    Because it's nice.

--- index.js
+++ index.js
@@ -11,5 +11,17 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier/@typescript-eslint',
 		'plugin:prettier/recommended'
-	]
+	],
+	rules: {
+		'import/order': [
+			'warn',
+			{
+				groups: [['builtin', 'external'], 'internal', ['parent', 'index', 'sibling']],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+				},
+			},
+		],
+	}
 }
